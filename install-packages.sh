8ce0d594e40dccc170c0fed7b6eccd0e31040824
thomas-svrts <81048302+thomas-svrts@users.noreply.github.com>
2025-06-17 00:18:08 +0200

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# 1. Repository toevoegen
 type -p curl >/dev/null || sudo apt install curl -y
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | \
   sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 
-# 2. APT-bron toevoegen
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] \
   https://cli.github.com/packages stable main" | \
   sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
-# 3. Installeren
 sudo apt update
 sudo apt install gh -y
 

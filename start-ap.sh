40b05565a808541ccae67d2baa72a836b451be0f
thomas-svrts <81048302+thomas-svrts@users.noreply.github.com>
2025-06-17 20:32:40 +0200

    Update start-ap.sh

--- start-ap.sh
+++ start-ap.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 echo "🚀 Starting access point on wlan0, sharing internet from eth0..."
-sudo lnxrouter --ap wlan0 Chiro-Heffen-Licht
+
+# Start lnxrouter (zonder --share als je internet niet wil delen)
+sudo lnxrouter \
+  --ap wlan0 Chiro-Heffen-Licht \
+  -g 10.10.0.1 \
+  -n \
+  --catch-dns \
+  --isolate-clients \
+  --country BE	\
+  --hostname chiro-ap
+
+
+# Wacht kort om interfaces te laten opstarten
+sleep 2
+
+# Alleen redirectregels voor captive portal
+sudo iptables -t nat -F
+sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80  -j DNAT --to-destination 10.10.0.1:80
+sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 443 -j REDIRECT --to-ports 80
+
+echo "✅ Captive portal draait op http://10.10.0.1"

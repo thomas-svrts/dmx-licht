4fb8bae1486a356d5205758e30051ceb6059185e
thomas-svrts <81048302+thomas-svrts@users.noreply.github.com>
2025-06-17 20:58:39 +0200

    Update start-ap.sh

--- start-ap.sh
+++ start-ap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Alleen redirectregels voor captive portal
+# sudo iptables -t nat -F
+sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80  -j DNAT --to-destination 10.10.0.1:80
+sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 443 -j REDIRECT --to-ports 80
+
+echo "✅ Captive portal draait op http://10.10.0.1"
+
+
 echo "🚀 Starting access point on wlan0, catching all dns..."
 
 # Start lnxrouter (zonder --share als je internet niet wil delen)
@@ -10,16 +18,7 @@ sudo lnxrouter \
   --catch-dns \
   --isolate-clients \
   --country BE	\
-  --hostname chiro-ap \
-  > lnxrouter.log 2>&1 &
+  --hostname chiro-ap
 
 
-# Wacht kort om interfaces te laten opstarten
-sleep 5
 
-# Alleen redirectregels voor captive portal
-# sudo iptables -t nat -F
-sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80  -j DNAT --to-destination 10.10.0.1:80
-sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 443 -j REDIRECT --to-ports 80
-
-echo "✅ Captive portal draait op http://10.10.0.1"

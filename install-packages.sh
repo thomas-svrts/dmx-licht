3dfedabf0ede5d75944ab168158716afd39b659f
thomas-svrts <81048302+thomas-svrts@users.noreply.github.com>
2025-06-27 21:11:33 +0200

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -140,3 +140,17 @@ sudo systemctl enable lighttpd
 sudo systemctl restart lighttpd
 
 echo "✅ lighttpd is ingesteld met rewrite en captive trigger-bestanden."
+
+
+
+# Get rpi-source
+sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source
+
+# Make it executable
+sudo chmod +x /usr/bin/rpi-source
+
+# Tell the update mechanism that this is the latest version of the script
+/usr/bin/rpi-source -q --tag-update
+
+# Get the kernel files thingies.
+rpi-source
